3989a10babd7c2ead69e2be3fa21741b64ecb267
zidane <huuvi168@gmail.com>
2014-10-29 09:28:22 +0700

    JSON demo
    how to access element from JSON files

---
+++ DemoJSON/DemoJSON/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DemoJSON
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.lsBox = new System.Windows.Forms.ListBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(49, 12);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(555, 20);
+            this.txtLink.TabIndex = 0;
+            // 
+            // lsBox
+            // 
+            this.lsBox.FormattingEnabled = true;
+            this.lsBox.Location = new System.Drawing.Point(49, 48);
+            this.lsBox.Name = "lsBox";
+            this.lsBox.Size = new System.Drawing.Size(555, 108);
+            this.lsBox.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(49, 178);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Execute";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(523, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 225);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lsBox);
+            this.Controls.Add(this.txtLink);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.ListBox lsBox;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DemoJSON/DemoJSON/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Dynamic;
+using System.Web.Helpers;   // phải check vào mới add -> ~.~
+
+namespace DemoJSON
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string str = txtLink.Text;
+            MessageBox.Show(GetWebServices(str));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyJSonParse();
+        }
+
+        private void MyJSonParse()
+        {
+            WebClient wb = new WebClient();
+
+            var data = "";
+
+            if (txtLink.Text == "")
+                data = "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo";
+            
+            else
+                data = txtLink.Text;
+
+            data = wb.DownloadString(data);
+
+
+            dynamic json;
+            if (txtLink.Text == "")
+            {
+                data = @"{ ""Name"": ""Jon Smith"", ""Address"": { ""City"": ""New York"", ""State"": ""NY"" }, ""Age"": 42 }";
+                json = System.Web.Helpers.Json.Decode(data);
+            }
+            else
+            {
+
+                
+                // ok
+                // data += "{\"flag\": 1, \"result\": { \"ITEM_ID\": \"CNSK\", \"NAME\": \"Cẩm nang sự kiện\",\"PRODUCT_CODE\": \"FS\",\"ID_INGAME\": \"6,1,5572\", \"TYPE\": \"1\", \"START_TIME\": null, \"END_TIME\": null } }";
+                
+                // data = "{ \"1\": { \"result\": {   \"ITEM_ID\":\"CNSK\", \"NAME\":\"C\u1ea9m nang s\u1ef1 ki\u1ec7n\", \"PRODUCT_CODE\":\"FS\", \"ID_INGAME\":\"6,1,5572\", \"TYPE\":\"1\", \"START_TIME\":null, \"END_TIME\":null} }, \"0\":1 }";
+                
+
+
+                // data = "{ \"results\": [ { \"SwiftCode\":\"\", \"City\": \"\", \"BankName\": \"Deutsche Bank\", \"Bankkey\":\"10020030\", \"Bankcountry\":\"DE\" }, { \"SwiftCode\":\"\", \"City\":\"10891 Berlin\", \"BankName\": \"Commerzbank Berlin (West)\", \"Bankkey\": \"10040000\", \"Bankcountry\":\"DE\" } ]}";
+
+
+                json = System.Web.Helpers.Json.Decode(data);
+            }
+
+            /*
+            {
+                "flag": 1,
+                "result": {
+                    "ITEM_ID": "CNSK",
+                    "NAME": "Cẩm nang sự kiện",
+                    "PRODUCT_CODE": "FS",
+                    "ID_INGAME": "6,1,5572",
+                    "TYPE": "1",
+                    "START_TIME": null,
+                    "END_TIME": null
+                }
+            }
+            
+            json.flag
+            json.result.ITEM_ID
+            
+            */
+
+            /*
+            {   "Name": "Jon Smith", 
+                "Address": 
+                { 
+                    "City": "New York", 
+                    "State": "NY" 
+                }, 
+                "Age": 42
+            }
+            -> json.Name
+            -> json.Address.City
+             
+            */
+
+            /*
+            {
+               "1": {
+                   "result":
+                    {  
+                        "ITEM_ID":"CNSK",
+                        "NAME":"C\u1ea9m nang s\u1ef1 ki\u1ec7n",
+                        "PRODUCT_CODE":"FS",
+                        "ID_INGAME":"6,1,5572",
+                        "TYPE":"1",
+                        "START_TIME":null,
+                        "END_TIME":null}
+                    },
+                "0":1
+            }
+             * 
+             -> json["1"].result.ITEM_ID = "CNSK" ;
+             -> json["1"].result.NAME = "Cẩm nang sự kiện";
+             -> json["1"].START_TIME = "";
+             */
+
+            /*
+            {
+                'results':[
+                    {
+                        'SwiftCode':'',
+                        'City':'',
+                        'BankName':'Deutsche    Bank',
+                        'Bankkey':'10020030',
+                        'Bankcountry':'DE'
+                    },
+                    {
+                        'SwiftCode':'',
+                        'City':'10891    Berlin',
+                        'BankName':'Commerzbank Berlin (West)',
+                        'Bankkey':'10040000',
+                        'Bankcountry':'DE'
+                    }
+            ]};
+             
+            -> json.results[0].SwiftCode = ""
+            -> json.results[0].BankName = "Deutsche    Bank"
+            -> json.results[1].BankKey = "10040000"
+            
+            */
+
+            
+            MessageBox.Show(json["1"].result.ITEM_ID);
+            MessageBox.Show(json["1"].result.NAME);
+            MessageBox.Show(json["1"].START_TIME);
+        }
+
+        private String GetWebServices(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch(WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(responStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = sr.ReadToEnd();
+                    // log errorText;
+                }
+                throw;
+            }
+        }       
+    }
+}
